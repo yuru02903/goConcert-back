65e3dd5f2f6fabb1d321ee650cd6c70d34d55d6e
yuru02903 <yuru02903@gmail.com>
2024-02-27 22:35:03 +0800

    update

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -1,6 +1,6 @@
 import tickets from '../models/tickets.js'
 import { StatusCodes } from 'http-status-codes'
-// import validator from 'validator'
+import validator from 'validator'
 
 export const create = async (req, res) => {
   try {
@@ -13,6 +13,161 @@ export const create = async (req, res) => {
     })
   } catch (error) {
     if (error.name === 'ValidationError') {
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message
+      })
+    } else {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: '未知錯誤'
+      })
+    }
+  }
+}
+
+export const getLaunchedTickets = async (req, res) => {
+  try {
+    const sortBy = req.query.sortBy || 'createdAt'
+    const sortOrder = parseInt(req.query.sortOrder) || -1
+    const itemsPerPage = parseInt(req.query.itemsPerPage) || 20
+    const page = parseInt(req.query.page) || 1
+    // RegExp( 來源,'i') => 將來源表達式，且不分大小寫(i)
+    const regex = new RegExp(req.query.search || '', 'i')
+    const data = await tickets
+      .find({
+        sell: true,
+        $or: [
+          { name: regex },
+          { performer: regex },
+          { categoryCountry: regex }
+        ]
+      })
+    // const text = 'a'
+    // const obj = { [text]: 1 }
+    // obj.a = 1
+      .sort({ [sortBy]: sortOrder })
+      .skip((page - 1) * itemsPerPage)
+      .limit(itemsPerPage === -1 ? undefined : itemsPerPage)
+
+    // estimatedDocumentCount() 計算總資料數
+    const total = await tickets.countDocuments({ sell: true })
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result: {
+        data, total
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: '未知錯誤'
+    })
+  }
+}
+
+export const getMyTickets = async (req, res) => {
+  try {
+    const sortBy = req.query.sortBy || 'createdAt'
+    const sortOrder = parseInt(req.query.sortOrder) || -1
+    const itemsPerPage = parseInt(req.query.itemsPerPage) || 10
+    const page = parseInt(req.query.page) || 1
+    // RegExp( 來源,'i') => 將來源表達式，且不分大小寫(i)
+    const regex = new RegExp(req.query.search || '', 'i')
+
+    const data = await tickets
+      .find({
+        seller: req.user._id,
+        $or: [
+          { name: regex },
+          { performer: regex },
+          { categoryCountry: regex }
+        ]
+      })
+      // const text = 'a'
+      // const obj = { [text]: 1 }
+      // obj.a = 1
+      .sort({ [sortBy]: sortOrder })
+      .skip((page - 1) * itemsPerPage)
+      .limit(itemsPerPage === -1 ? undefined : itemsPerPage)
+
+    const total = await tickets.countDocuments({ seller: req.user._id })
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result: {
+        data, total
+      }
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: '未知錯誤'
+    })
+  }
+}
+
+export const getTicketId = async (req, res) => {
+  try {
+    // 判斷網址參數ID的格式是否正確
+    if (!validator.isMongoId(req.params.id)) throw new Error('ID')
+
+    const result = await tickets.findById(req.params.id)
+
+    if (!result) throw new Error('ID NOT FOUND')
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result
+    })
+  } catch (error) {
+    console.log(error)
+    if (error.name === 'CastError' || error.message === 'ID') {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: 'ID 格式錯誤'
+      })
+    } else if (error.message === 'ID NOT FOUND') {
+      res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: '查無票券'
+      })
+    } else {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: '未知錯誤'
+      })
+    }
+  }
+}
+
+export const edit = async (req, res) => {
+  try {
+    if (!validator.isMongoId(req.params.id)) throw new Error('ID')
+    await tickets.findByIdAndUpdate(req.params.id, req.body, { runValidators: true }).orFail(new Error('NOT FOUND'))
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: ''
+    })
+  } catch (error) {
+    if (error.name === 'CastError' || error.message === 'ID') {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: 'ID 格式錯誤'
+      })
+    } else if (error.message === 'NOT FOUND') {
+      res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: '查無票券'
+      })
+    } else if (error.name === 'ValidationError') {
       const key = Object.keys(error.errors)[0]
       const message = error.errors[key].message
       res.status(StatusCodes.BAD_REQUEST).json({
